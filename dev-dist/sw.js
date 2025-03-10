4d1a4c043ba5f7439f5ad3a750b9363ccf9f178a
Austin Obimbo <obimboausts@gmail.com>
2025-03-10 15:05:57 +0300

    checkout session create

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hho62olmj4o"
+    "revision": "0.4o7oo7b4mh8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
